e88682ee1b7b897a75e5fb07f16263108b6ed318
Beryl <281785651+beryllus@users.noreply.github.com>
2026-06-24 16:25:01 -0500

    Get random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("test")
+const getComputerChoice = () => {
+    const randomNumber = getRandomNumber(2);
+    if (randomNumber === 0){
+        return "rock";
+    } else if (randomNumber === 1){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+const getRandomNumber = (max) => {
+    return Math.floor(Math.random() * max);
+}
