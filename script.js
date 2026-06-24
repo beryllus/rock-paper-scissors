826def01734de90bf9a4601933b50d761d5ee252
Beryl <281785651+beryllus@users.noreply.github.com>
2026-06-24 16:29:59 -0500

    Get human choice

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const getComputerChoice = () => {
     const randomNumber = getRandomNumber(2);
-    if (randomNumber === 0){
+    if (randomNumber === 0) {
         return "rock";
-    } else if (randomNumber === 1){
+    } else if (randomNumber === 1) {
         return "paper";
     } else {
         return "scissors";
@@ -12,3 +12,13 @@ const getComputerChoice = () => {
 const getRandomNumber = (max) => {
     return Math.floor(Math.random() * max);
 }
+
+const getHumanChoice = () => {
+    let input;
+    do {
+        input = prompt("Rock, paper or scissors?").toLowerCase();
+    } while (input !== "rock" && input !== "scissors" && input !== "paper");
+    return input;
+}
+
+console.log(getHumanChoice());
