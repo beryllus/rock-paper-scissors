60f275bf2454303df1689643a909f3a3cc419bfb
Beryl <281785651+beryllus@users.noreply.github.com>
2026-06-24 16:50:27 -0500

    Play individual round

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 const getComputerChoice = () => {
-    const randomNumber = getRandomNumber(2);
+    const randomNumber = getRandomNumber(3);
     if (randomNumber === 0) {
         return "rock";
     } else if (randomNumber === 1) {
@@ -23,3 +23,20 @@ const getHumanChoice = () => {
     } while (input !== "rock" && input !== "scissors" && input !== "paper");
     return input;
 }
+
+const playRound = (humanChoice, computerChoice) => {
+    console.log(computerChoice);
+    const choices = ["rock", "paper", "scissors", "rock", "paper"];
+    const computerIndex = choices.indexOf(computerChoice);
+    if (humanChoice === computerChoice) {
+        console.log("Draw");
+    } else if (choices[computerIndex+2] === humanChoice) {
+        console.log("Computer wins!");
+        computerScore++;
+    } else {
+        console.log("Human wins!");
+        humanScore++;
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
